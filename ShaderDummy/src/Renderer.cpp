ac0a0cce5a2786c84be8d8aa8f6ad79c3038f4b2
Alessio Paoletti <alessiopaoletti32@gmail.com>
2019-06-08 13:59:14 +0100

    Abstracted Renderer functionality.

--- ShaderDummy/src/Renderer.cpp
+++ ShaderDummy/src/Renderer.cpp
@@ -1,11 +1,12 @@
 #include "Renderer.h"
 
+#include <iostream>
+
 
 void GLClearError()
 {
     while (glGetError() != GL_NO_ERROR);
 }
-
 
 bool GLLogCall(const char* function, const char* file, int line)
 {
@@ -16,4 +17,19 @@ bool GLLogCall(const char* function, const char* file, int line)
         return false;
     }
     return true;
+}
+
+void Renderer::Clear() const
+{
+    GLCall(glClear(GL_COLOR_BUFFER_BIT));
+}
+
+void Renderer::Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const
+{
+    va.Bind();
+    ib.Bind();
+    shader.Bind();
+
+    // Draw call - passing nullptr because we are already selecting the index buffer
+    GLCall(glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr)); // Assuming here that the ib is of type GL_UNSIGNED_INT
 }
--- ShaderDummy/src/Renderer.h
+++ ShaderDummy/src/Renderer.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <GL/glew.h>
-#include <iostream>
+
+#include "VertexArray.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
+
 
 // __debugbreak is platfrom specific here
 #define ASSERT(x) if (!(x)) __debugbreak();
@@ -18,3 +22,11 @@
 void GLClearError();
 
 bool GLLogCall(const char* function, const char* file, int line);
+
+
+class Renderer
+{
+public:
+    void Clear() const;
+    void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+};
